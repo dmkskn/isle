e647d009e3793e6493c6b3f4af8f1510a74c67f3
dmkskn <dmksknn@gmail.com>
2018-09-25 17:56:18 +0300

    Initinal commit

---
+++ tests/test_api.py
@@ -0,0 +1,46 @@
+import unittest
+
+
+
+class TMDbAPITestCase(unittest.TestCase):
+    pass
+
+
+
+class SearchMovieTestCase(unittest.TestCase):
+    pass
+
+
+
+class SearchShowTestCase(unittest.TestCase):
+    pass
+
+
+
+class SearchPersonTestCase(unittest.TestCase):
+    pass
+
+
+
+class SearchCompanyTestCase(unittest.TestCase):
+    pass
+
+
+
+class DiscoverMovieTestCase(unittest.TestCase):
+    pass
+
+
+
+class DiscoverShowTestCase(unittest.TestCase):
+    pass
+
+
+
+class DiscoverPersonTestCase(unittest.TestCase):
+    pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_objects.py
@@ -0,0 +1,26 @@
+import unittest
+
+
+
+class MovieTestCase(unittest.TestCase):
+    pass
+
+
+
+class ShowTestCase(unittest.TestCase):
+    pass
+
+
+
+class PersonTestCase(unittest.TestCase):
+    pass
+
+
+
+class CompanyTestCase(unittest.TestCase):
+    pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
