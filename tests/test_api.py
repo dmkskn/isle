2d275f4b95ac94f74c83d8cedddffb635f510086
dmkskn <dmksknn@gmail.com>
2018-10-04 16:55:36 +0300

    Add DiscoverShowTestCase

--- tests/test_api.py
+++ tests/test_api.py
@@ -244,7 +244,34 @@ class DiscoverMovieTestCase(unittest.TestCase):
 
 
 class DiscoverShowTestCase(unittest.TestCase):
-    pass
+    BASEURL = "https://api.themoviedb.org/3/discover/tv?"
+    
+    @classmethod
+    def setUpClass(cls):
+        options = {
+            'sort_by': 'popularity.desc',
+            'with_companies': 278, # Propaganda Films
+        }
+        cls.results = src.discover_shows(options)
+        cls.results_list = list(cls.results)
+        cls.api_response = cls.get_api_response(options)
+        cls.total_results = cls.api_response["total_results"]
+    
+    @classmethod
+    def get_api_response(cls, options):
+        params = {"api_key": os.environ["TMDB_API_KEY"], **options}
+        params = urlencode(params)
+        response = urlopen(f"{cls.BASEURL}{params}")
+        return json.loads(response.read().decode("utf-8"))
+
+    def test_output_is_generator(self):
+        self.assertTrue(inspect.isgenerator(self.results))
+
+    def test_output_item_is_Show_instance(self):
+        self.assertIsInstance(self.results_list[0], src.Show)
+
+    def test_amount_of_results(self):
+        self.assertEqual(len(self.results_list), self.total_results)
 
 
 
