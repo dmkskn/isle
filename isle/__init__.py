583782755a78cf9024c2cdf2f4705fbbee27242c
dmkskn <dmksknn@gmail.com>
2018-12-24 16:20:23 +0300

    Add __all__ and the definition to isle.__init__

--- isle/__init__.py
+++ isle/__init__.py
@@ -1,7 +1,13 @@
+"""A wrapper for The Movie Database API"""
 import os
+
+import isle._api
 
 from .objects import *
 from ._api import *
+
+
+__all__ = isle._api.__all__
 
 
 TMDB_API_KEY = os.environ.get("TMDB_API_KEY", None)
